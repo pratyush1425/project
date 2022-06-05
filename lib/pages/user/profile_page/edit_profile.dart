5bfd437dca0f84c6a7fb7b5831d2479d33418c77
Pratyush1425 <pratyush14102001@gmail.com>
2022-06-05 14:23:38 +0530

    edited edit profile

--- lib/pages/user/profile_page/edit_profile.dart
+++ lib/pages/user/profile_page/edit_profile.dart
@@ -4,128 +4,103 @@ import 'package:flutter/material.dart';
 import 'package:project/common/bottom_bar.dart';
 import 'package:project/common/drawer.dart';
 
-class Editprofile extends StatelessWidget {
+class Editprofile extends StatefulWidget {
   const Editprofile({Key? key}) : super(key: key);
 
   @override
+  State<Editprofile> createState() => _EditprofileState();
+}
+
+class _EditprofileState extends State<Editprofile> {
+  String _email = "";
+  String _password = "";
+  bool flag = true;
+  String message = "";
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
+    //double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("Edit Profile"),
       ),
       drawer: MyDrawer(),
-      body: Container(
-        child: Column(children: [
-          SizedBox(height: 25),
-          Text(
-            "EDIT PROFILE",
-            style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 20.0,
-                color: Color.fromARGB(255, 41, 63, 77)),
-          ),
-          SizedBox(height: 25),
-          Row(
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-                child: Container(
-                  width: 70,
+              const Padding(
+                  padding: EdgeInsets.all(8.0),
                   child: Text(
-                    "Name",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
+                    "Sign In",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  )),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 235, 235, 235),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Enter",
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: "Email",
+                            border: OutlineInputBorder(),
+                            hintText: "Enter your email"),
+                        validator: (value) {
+                          if (value!.isEmpty ||
+                              !value.contains('@') ||
+                              !value.contains('.')) {
+                            return "Please enter correct email";
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _email = value!;
+                        },
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: "Name",
+                            border: OutlineInputBorder(),
+                            hintText: "Enter your name"),
+                        validator: (value) {
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _password = value!;
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    Text(
+                      message,
+                      style: TextStyle(color: Color.fromARGB(255, 209, 30, 17)),
+                    ),
+                    const SizedBox(height: 25),
+                    ElevatedButton(
+                        child: const Text(
+                          "Submit",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () async {
+                          _formkey.currentState!.validate();
+                          _formkey.currentState!.save();
+                        }),
+                  ],
                 ),
               ),
             ],
           ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-                child: Container(
-                  width: 70,
-                  child: Text(
-                    "Roll no",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 235, 235, 235),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Enter",
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-                child: Container(
-                  width: 70,
-                  child: Text(
-                    "Email ID",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 235, 235, 235),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Enter",
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )
-        ]),
+        ),
       ),
       bottomNavigationBar: MyBottomBar(),
     );
