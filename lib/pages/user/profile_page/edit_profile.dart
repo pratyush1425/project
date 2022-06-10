e93dfaede630d2cfdcf2dc5fbe0a70caf78c9d31
Prabsambhi <prabsambhi@gmail.com>
2022-06-11 00:04:37 +0530

    hiii

--- lib/pages/user/profile_page/edit_profile.dart
+++ lib/pages/user/profile_page/edit_profile.dart
@@ -25,178 +25,146 @@ class Editprofile extends State<EditProfile> {
     var size = MediaQuery.of(context).size.width;
     var h = MediaQuery.of(context).size.height;
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      //   leading: IconButton(
-      //     icon: Icon(
-      //       Icons.arrow_back,
-      //       color: Color.fromARGB(255, 76, 165, 175),
-      //     ),
-      //     onPressed: () {
-      //       Navigator.pop(context);
-      //       Navigator.pushNamed(context, '/profile');
-      //     },
-      //   ),
-      // ),
-      body: Container(
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: ListView(
+      body: ListView(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, '/profile');
+            },
+            child: Expanded(
+              child: Container(
+                margin:
+                    const EdgeInsets.only(left: 10.0, top: 10.0, bottom: 10.0),
+                alignment: Alignment.topLeft,
+                child: Icon(
+                  Icons.arrow_back,
+                  size: 30,
+                ),
+              ),
+            ),
+          ),
+          Stack(
             children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, '/profile');
-                },
-                child: Expanded(
+              Container(
+                width: size,
+                height: 200,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWL6HWblTMbGEk_9c0lMxbCP_QWE8p_lXotg&usqp=CAU",
+                        ))),
+              ),
+              Positioned(
+                left: size / 3,
+                top: h / 24,
+                child: Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 4,
+                          color: Theme.of(context).scaffoldBackgroundColor),
+                      boxShadow: [
+                        BoxShadow(
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.3),
+                            offset: Offset(0, 5))
+                      ],
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            photourl,
+                          ))),
+                ),
+              ),
+              Positioned(
+                  right: size / 3,
+                  top: h / 5.5,
                   child: Container(
-                    margin: const EdgeInsets.only(left: 10.0, top: 10.0),
-                    alignment: Alignment.topLeft,
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        width: 4,
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                      ),
+                      color: Color.fromARGB(255, 76, 165, 175),
+                    ),
                     child: Icon(
-                      Icons.arrow_back,
-                      size: 30,
+                      Icons.edit,
+                      color: Colors.white,
                     ),
+                  )),
+            ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          buildTextField("Full Name", name, false),
+          buildTextField("E-mail", email, false),
+          buildTextField("Password", password, true),
+          buildTextField("Phone", phone, false),
+          buildTextField("Address", address, false),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(14, 0, 0, 30),
+                width: 120,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, '/edit_profile');
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    )),
+                  ),
+                  child: Text(
+                    "CANCEL",
+                    style: TextStyle(
+                        fontSize: 14, letterSpacing: 2.2, color: Colors.white),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 15,
-              ),
-              Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      width: size,
-                      height: 200,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWL6HWblTMbGEk_9c0lMxbCP_QWE8p_lXotg&usqp=CAU",
-                              ))),
-                    ),
-                    Positioned(
-                      left: size / 3,
-                      top: h / 24,
-                      child: Container(
-                        width: 140,
-                        height: 140,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 4,
-                                color:
-                                    Theme.of(context).scaffoldBackgroundColor),
-                            boxShadow: [
-                              BoxShadow(
-                                  spreadRadius: 1,
-                                  blurRadius: 10,
-                                  color: Colors.black.withOpacity(0.3),
-                                  offset: Offset(0, 5))
-                            ],
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(
-                                  photourl,
-                                ))),
-                      ),
-                    ),
-                    Positioned(
-                        right: size / 3,
-                        top: h / 5.5,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              width: 4,
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                            ),
-                            color: Color.fromARGB(255, 76, 165, 175),
-                          ),
-                          child: Icon(
-                            Icons.edit,
-                            color: Colors.white,
-                          ),
-                        )),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              buildTextField("Full Name", name, false),
-              buildTextField("E-mail", email, false),
-              buildTextField("Password", password, true),
-              buildTextField("Phone", phone, false),
-              buildTextField("Address", address, false),
-              SizedBox(
-                height: 35,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(14, 0, 0, 0),
-                    width: 120,
-                    height: 40,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        Navigator.pushNamed(context, '/edit_profile');
-                      },
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        )),
-                      ),
-                      child: Text(
-                        "CANCEL",
-                        style: TextStyle(
-                            fontSize: 14,
-                            letterSpacing: 2.2,
-                            color: Colors.white),
-                      ),
-                    ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 14, 30),
+                width: 120,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    // await FirebaseAuth.instance.currentUser?.updateDisplayName("Jane Q. User");
+                    // await FirebaseAuth.instance.currentUser?.updateEmail("janeq@example.com");
+                    // await FirebaseAuth.instance.currentUser?.sendEmailVerification();
+                    // await FirebaseAuth.instance.currentUser?.updatePhoneNumber(phoneCredential);
+                    // await FirebaseAuth.instance.currentUser?.verifyBeforeUpdateEmail(newEmail);
+                    //
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    )),
                   ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 16, 0),
-                    width: 120,
-                    height: 40,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        // await FirebaseAuth.instance.currentUser?.updateDisplayName("Jane Q. User");
-                        // await FirebaseAuth.instance.currentUser?.updateEmail("janeq@example.com");
-                        // await FirebaseAuth.instance.currentUser?.sendEmailVerification();
-                        // await FirebaseAuth.instance.currentUser?.updatePhoneNumber(phoneCredential);
-                        // await FirebaseAuth.instance.currentUser?.verifyBeforeUpdateEmail(newEmail);
-                        //
-                      },
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        )),
-                      ),
-                      child: Text(
-                        "SAVE",
-                        style: TextStyle(
-                            fontSize: 14,
-                            letterSpacing: 2.2,
-                            color: Colors.white),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 35,
+                  child: Text(
+                    "SAVE",
+                    style: TextStyle(
+                        fontSize: 14, letterSpacing: 2.2, color: Colors.white),
+                  ),
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
@@ -205,7 +173,6 @@ class Editprofile extends State<EditProfile> {
     return Padding(
       padding: EdgeInsets.only(bottom: 35.0, left: 16, right: 16),
       child: TextField(
-        // controller: placeholder,
         obscureText: isPasswordTextField ? showPassword : false,
         decoration: InputDecoration(
             suffixIcon: isPasswordTextField
--- lib/pages/user/profile_page/profile.dart
+++ lib/pages/user/profile_page/profile.dart
@@ -1,108 +1,3 @@
-// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-// import 'package:flutter/material.dart';
-// import 'package:project/common/bottom_bar.dart';
-// import 'package:project/common/drawer.dart';
-
-// class Profile extends StatelessWidget {
-//   const Profile({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     var size = MediaQuery.of(context).size;
-//     var username = "Prabhjot Singh";
-//     var noOfFriends = 70;
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Profile"),
-//       ),
-//       drawer: MyDrawer(),
-//       body: Container(
-//         height: size.height * 0.33,
-//         color: Color.fromRGBO(79, 147, 255, 1),
-//         child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-//           Column(
-//             children: [
-//               Container(
-//                   margin: const EdgeInsets.only(top: 25.0),
-//                   height: 80,
-//                   decoration: BoxDecoration(
-//                     border:
-//                         Border.all(color: Color.fromARGB(255, 255, 255, 255)),
-//                     color: Color.fromARGB(255, 254, 254, 254),
-//                     shape: BoxShape.circle,
-//                     image: DecorationImage(
-//                       image: NetworkImage(
-//                           'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png'),
-//                     ),
-//                   )),
-//               Text(username,
-//                   style: TextStyle(
-//                       fontSize: 20.0,
-//                       color: Color.fromARGB(255, 255, 255, 255)))
-//             ],
-//           ),
-//           SizedBox(height: 25),
-//           Row(
-//             children: <Widget>[
-//               // Expanded(
-//               Expanded(
-//                 child: OutlinedButton(
-//                     onPressed: () {},
-//                     style: OutlinedButton.styleFrom(
-//                         side: BorderSide(
-//                       color: Colors.transparent,
-//                     )),
-//                     child: Column(children: [
-//                       Text(
-//                         "$noOfFriends",
-//                         style: TextStyle(
-//                             fontWeight: FontWeight.w700,
-//                             fontSize: 25.0,
-//                             color: Color.fromARGB(255, 255, 255, 255)),
-//                       ),
-//                       Text(
-//                         "Following",
-//                         style: TextStyle(
-//                             fontWeight: FontWeight.w300,
-//                             fontSize: 20.0,
-//                             color: Color.fromARGB(255, 255, 255, 255)),
-//                       )
-//                     ])),
-//               ),
-//               Expanded(
-//                 child: OutlinedButton(
-//                   onPressed: () {
-//                     Navigator.pop(context);
-//                     Navigator.pushNamed(context, '/edit_profile');
-//                   },
-//                   style: OutlinedButton.styleFrom(
-//                       padding: EdgeInsets.all(5),
-//                       side: BorderSide(
-//                         color: Colors.transparent,
-//                       )),
-//                   child: Text(
-//                     "EDIT PROFILE",
-//                     style: TextStyle(
-//                         fontSize: 20.0,
-//                         fontWeight: FontWeight.w300,
-//                         color: Color.fromARGB(255, 255, 255, 255)),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           )
-//         ]),
-//       ),
-//       bottomNavigationBar: MyBottomBar(),
-//     );
-//   }
-// }
-
-// ignore_for_file: prefer_const_constructors
-
-// import 'dart:js';
-
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -117,19 +12,6 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffdee2fe),
-      // appBar: AppBar(
-      //   backgroundColor: const Color(0xffdee2fe),
-      //   leading: IconButton(
-      //     icon: Icon(
-      //       Icons.edit,
-      //       color: Color.fromARGB(255, 76, 165, 175),
-      //     ),
-      //     onPressed: () {
-      //       Navigator.pop(context);
-      //       Navigator.pushNamed(context, '/edit_profile');
-      //     },
-      //   ),
-      // ),
       body: SizedBox(
         width: double.infinity,
         child: ListView(
@@ -213,6 +95,8 @@ class _ProfileState extends State<Profile> {
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(50),
                     topRight: Radius.circular(50),
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
                   ),
                 ),
                 child: Column(
@@ -231,14 +115,14 @@ class _ProfileState extends State<Profile> {
                       height: 16,
                     ),
                     listProfile(Icons.person, "Full Name", "Kyle Anderson"),
+                    listProfile(Icons.person_pin, "Roll No", "2K19/MC/155"),
                     listProfile(
                         Icons.date_range, "Date of Birth", "July 21, 2000"),
-                    listProfile(Icons.location_pin, "Location",
-                        "California, United States"),
                     listProfile(Icons.male, "Gender", "Male"),
-                    listProfile(Icons.phone, "Phone Number", "15555228"),
-                    listProfile(Icons.phone, "Phone Number", "15555228"),
-                    listProfile(Icons.phone, "Phone Number", "15555228"),
+                    listProfile(Icons.mail, "Email Id", "kyle@gmail.com"),
+                    listProfile(Icons.phone, "Phone Number", "9212939492"),
+                    listProfile(Icons.location_pin, "Address",
+                        "California, United States"),
                   ],
                 ),
               ),
